2e795e012a77ec57159397cb77b315e82c15b0e1
Melvin Teo <mthteo@gmail.com>
2021-06-28 11:13:42 -0700

    finished library, but max books is 15 before overflow

--- js/script.js
+++ js/script.js
@@ -17,22 +17,22 @@ gotData = (data) => {
     for (let i = 0; i < cards.length; i++) {
         cards[i].remove();
     }
+    BOOKNUMBERS = 0;
 
     // books is the parent object of long str key objects
     let books = data.val();
-    // gets array of all the object keys
-    let keys = Object.keys(books);
-    console.log(keys);
-    console.log(keys.length);
-    for (let i = 0; i < keys.length; i++) {
-        let key = keys[i];
-        let title = books[key].title;
-        let pages = books[key].pages;
-        let author = books[key].author;
-        let read = books[key].read;
-        console.log(keys[i]);
-        console.log("add-book");
-        addBookToLibrary(title, pages, author, read, key);
+    // if there are actually books uploaded
+    if (books != undefined) {
+        // gets array of all the object keys
+        let keys = Object.keys(books);
+        for (let i = 0; i < keys.length; i++) {
+            let key = keys[i];
+            let title = books[key].title;
+            let pages = books[key].pages;
+            let author = books[key].author;
+            let read = books[key].read;
+            addBookToLibrary(title, pages, author, read, key);
+        }
     }
 }
 
@@ -45,24 +45,6 @@ errData = (data) => {
 var booksRef = firebase.database().ref('books');
 booksRef.on('value', gotData, errData);
 
-// get elements
-// const preObject = document.getElementById('object');
-
-// create references
-// ref -> root
-// child -> creates child key of object
-// const dbRefObject = firebase.database().ref().child('book');
-
-
-// sync object data changes
-// called everytime there is data change
-// snap param = data snapshot which returns important things
-// dbRefObject.on('value', snap => {
-//     preObject.innerHTML = JSON.stringify(snap.val(), null, 3);
-// });
-
-// project id library-bd1e
-
 const modal = document.querySelector('.modal');
 const modalForm = document.getElementById('modal-form');
 const container = document.querySelector('.container');
@@ -72,87 +54,66 @@ const exitBtn = document.getElementById('exit-btn');
 const form = document.querySelector('.modal-form');
 const cancelBtn = document.getElementById('cancel-btn');
 
-// array to hold books
-// let books = [];
+let BOOKNUMBERS = 0;
 
-// constructor function which creates book objects
-function Book(title, pages, author, read) {
-    this.title = title;
-    this.pages = pages;
-    this.author = author;
-    this.read = read;
-}
-
-// creates prototype function to change read status of object
-Book.prototype.changeReadStatus = function (readStatus) {
-    if (readStatus === 'read') {
-        this.read = 'unread';
-    } else {
-        this.read = 'read';
-    }
-}
-
-// consider looping through form.elements?
+// adds new card to the container div
 addBookToLibrary = (title, pages, author, readStatus, key) => {
-    // let title = getInputValue('book-title');
-    // let pages = getInputValue('book-pages');
-    // let author = getInputValue('book-author');
-    // let read = document.getElementById('book-read');
-    // let readCheck = '';
+    BOOKNUMBERS += 1;
+    console.log(BOOKNUMBERS);
 
-    // if (read.checked) {
-    //     readCheck = 'read';
-    // } else {
-    //     readCheck = 'unread';
-    // }
-
-    // show confirmation
-    document.querySelector('.alert').style.display = 'block';
-
-    // Hide confirmation after 3s
-    setTimeout(() => {
-        document.querySelector('.alert').style.display = 'none';
-    }, 3000);
-
-    // creates new book object with user form data
-    myBook = new Book(title, pages, author, readStatus);
-    // books.push(myBook);
-
-    let info = document.createElement('p');
     let book = document.createElement('div');
-    book.classList.add('card');
 
-    for (const prop in myBook) {
-        if (typeof myBook[prop] !== 'function' && prop !== 'read') {
-            info.innerHTML += myBook[prop] + ' ';
-        }
-    }
-    book.appendChild(info);
+    let titleTag = document.createElement('p');
+    let pagesTag = document.createElement('p');
+    let authorTag = document.createElement('p');
+
+    let titleContent = document.createElement('p');
+    titleContent.classList.add('titleContent');
+    let pagesContent = document.createElement('p');
+    pagesContent.classList.add('pagesContent');
+    let authorContent = document.createElement('p');
+    authorContent.classList.add('authorContent');
+
+    let titleDiv = document.createElement('div');
+    titleDiv.classList.add('titleDiv');
+    let pagesDiv = document.createElement('div');
+    pagesDiv.classList.add('pagesDiv');
+    let authorDiv = document.createElement('div');
+    authorDiv.classList.add('authorDiv');
+
+    book.classList.add('card');
+    titleTag.innerHTML = 'Title:';
+    pagesTag.innerHTML = 'Pages:';
+    authorTag.innerHTML = 'Author:';
+
+    titleContent.innerHTML = `${title}`;
+    pagesContent.innerHTML = `${pages}`;
+    authorContent.innerHTML = `${author}`;
+
+    titleDiv.appendChild(titleTag);
+    titleDiv.appendChild(titleContent);
+    pagesDiv.appendChild(pagesTag);
+    pagesDiv.appendChild(pagesContent);
+    authorDiv.appendChild(authorTag);
+    authorDiv.appendChild(authorContent);
+
+    book.appendChild(titleDiv);
+    book.appendChild(pagesDiv);
+    book.appendChild(authorDiv);
 
     // add the book's array index number to keep track of it
     book.dataset.key = key;
     // book.dataset.indexNumber = books.length - 1;
 
-    // creates remove button
-    let removeBtn = document.createElement('button');
-    removeBtn.classList.add('remove-btn');
-    removeBtn.innerHTML = 'Remove Entry';
-    removeBtn.addEventListener('click', (ev) => {
-        // remove the child node of the books node that has the key of the variable key
-        let key = ev.target.parentElement.dataset.key;
-        booksRef.child(key).remove();
-        // let index = ev.target.parentElement.dataset.indexNumber;
-        // books.splice(index, 1);
-        // container.removeChild(ev.target.parentElement);
-        // decreaseIndex(index);
-    })
+    let readStatusDiv = document.createElement('div');
+    readStatusDiv.classList.add('readStatusDiv');
 
     // creates read/unread button
     let readBtn = document.createElement('button');
     readBtn.classList.add('read-btn');
     readBtn.innerHTML = 'Change Read Status';
     readBtn.addEventListener('click', (ev) => {
-        let key = ev.target.parentElement.dataset.key;
+        let key = ev.target.parentElement.parentElement.dataset.key;
 
         // update database to change read status
         if (readStatus === 'read') {
@@ -160,17 +121,9 @@ addBookToLibrary = (title, pages, author, readStatus, key) => {
         } else {
             booksRef.child(key).update({ 'read': 'read' });
         }
-
-        // let index = ev.target.parentElement.dataset.indexNumber;
-        // let book = books[index];
-        // let readCheckbox = document.getElementById(`read-checkbox-${index}`)
-        // book.changeReadStatus(book['read']);
-        // if (book['read'] === 'unread') {
-        //     readCheckbox.checked = false;
-        // } else {
-        //     readCheckbox.checked = true;
-        // }
     })
+
+    readStatusDiv.appendChild(readBtn);
 
     // creates read/unread checkbox
     let readCheckbox = document.createElement('input');
@@ -184,9 +137,26 @@ addBookToLibrary = (title, pages, author, readStatus, key) => {
         readCheckbox.checked = true;
     }
 
-    book.appendChild(removeBtn);
-    book.appendChild(readBtn);
-    book.appendChild(readCheckbox);
+    readStatusDiv.appendChild(readCheckbox);
+
+    let removeBtnDiv = document.createElement('div');
+    removeBtnDiv.classList.add('removeBtnDiv');
+
+    // creates remove button
+    let removeBtn = document.createElement('button');
+    removeBtn.classList.add('remove-btn');
+    removeBtn.innerHTML = 'Remove Entry';
+    removeBtn.addEventListener('click', (ev) => {
+        // remove the child node of the books node that has the key of the variable key
+        let key = ev.target.parentElement.parentElement.dataset.key;
+        booksRef.child(key).remove();
+        BOOKNUMBERS -= 1;
+    })
+
+    removeBtnDiv.appendChild(removeBtn);
+
+    book.appendChild(readStatusDiv);
+    book.appendChild(removeBtnDiv);
     container.appendChild(book);
 }
 
@@ -226,19 +196,33 @@ cancelBtn.addEventListener('click', () => {
 
 // check if all form sections are filled in
 validateForm = (form) => {
-    for (let i = 0; i < form.length - 2; i++) {
-        if (form[i].value === '') {
-            return false;
-        }
-    }
-    if (document.querySelector('input[name="book-read"]:checked') == null) {
-        return false;
-    }
+    // for (let i = 0; i < form.length - 2; i++) {
+    //     if (form[i].value === '') {
+    //         alert('Please fill out all the inputs!');
+    //         return false;
+    //     }
+    // }
+    // if (document.querySelector('input[name="book-read"]:checked') == null) {
+    //     alert('Please choose a read or unread option!');
+    //     return false;
+    // }
     return true;
 }
 
 modalForm.addEventListener('submit', (ev) => {
-    if (ev.submitter.id === 'submit-btn') {
+    if (BOOKNUMBERS >= 15 && BOOKNUMBERS % 15 === 0) {
+        let pageNumber = BOOKNUMBERS / 15;
+        console.log(pageNumber);
+        alert('Maximum books reached!');
+        return false;
+        // add button for page 2
+        // creates a new html page (page[x].html);
+        // use JS to create the same header and add new book button
+        // change all current ids -> classes
+        // link to another html page with new table
+    }
+    // otherwise check if booknumbers < 15, so remove the html page
+    if (ev.submitter.id === 'submit-btn' && validateForm(modalForm)) {
         ev.preventDefault();
         modal.style.display = 'none';
         // uploads data to firebase
@@ -253,7 +237,17 @@ modalForm.addEventListener('submit', (ev) => {
         } else {
             readStatus = 'unread';
         }
+
         saveMessage(title, pages, author, readStatus)
+        // show confirmation
+        document.querySelector('.alert').style.display = 'block';
+
+        // Hide confirmation after 3s
+        setTimeout(() => {
+            document.querySelector('.alert').style.display = 'none';
+        }, 3000);
+    } else {
+        return false;
     }
     form.reset();
 })
